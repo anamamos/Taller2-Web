2f62a692f46d59ad9ffda5bcbad1c30ca7aa11da
anamamos <anama.mos99@gmail.com>
2020-04-29 19:28:39 -0500

    Server

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
     console.log('hola en la consola');
-    res.send('hola en opera');
+    // res.send('hola mami linda');
+
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/contacto', function (req, res) {
@@ -12,7 +17,9 @@ app.get('/contacto', function (req, res) {
     res.send('pagina de contacto');
 });
 
-
+app.get('/sobre-nosotros', function (req, res) {
+    res.send('Texto bien makia');
+});
 
 app.listen(3000, function () {
     console.log('servidor iniciado en puerto 3000');
